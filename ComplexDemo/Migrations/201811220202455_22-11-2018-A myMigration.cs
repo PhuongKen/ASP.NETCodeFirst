fa45b29dd48969797ddb5adb5bc343911b0f5c4c
phuongken <you@example.com>
2018-12-01 22:26:48 +0700

    FirstPost

---
+++ ComplexDemo/Migrations/201811220202455_22-11-2018-A myMigration.cs
@@ -0,0 +1,16 @@
+namespace ComplexDemo.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class _22112018AmyMigration : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
